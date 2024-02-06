74da416ff9374d8efa5a9f9ff6506d3f224dfc1b
AbylaiAbdilda <abylaiabdilda@gmail.com>
2024-02-06 15:36:10 +0600

    Lab2

---
+++ Lab2/Booleans.py
@@ -0,0 +1,14 @@
+# EX 1
+print(10 > 9) #output : True
+
+#EX 2
+print(10 == 9 ) #output : False
+
+#EX 3 
+print(10 < 9 ) # output : False
+
+#EX 4
+print(bool("abc")) #output : True
+
+#EX 5
+print(bool(0)) #output: False
